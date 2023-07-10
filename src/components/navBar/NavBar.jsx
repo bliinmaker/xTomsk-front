484d3f31bfabd9744e127ab0f6702b3833650717
bliinmaker <kir05062005@gmail.com>
2023-07-10 23:48:32 +0300

    Change NavBar.scss file for the project.

--- src/components/navBar/NavBar.jsx
+++ src/components/navBar/NavBar.jsx
@@ -1,51 +1,55 @@
 import fontawesome from '@fortawesome/fontawesome'
-import { faBars, faTimes } from '@fortawesome/free-solid-svg-icons'
+import { faBars, faTimes, faXmark } from '@fortawesome/free-solid-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { useState } from 'react'
 import { Link } from 'react-router-dom'
 import './NavBar.scss'
 
-fontawesome.library.add(faBars, faTimes)
+fontawesome.library.add(faBars, faTimes, faXmark)
 
 export const NavBar = () => {
 	const [isNavExpanded, setIsNavExpanded] = useState(false)
 
-
-	return (<nav className="navigation">
-		<Link to={`/`} className='navbar-site-brand'>
-			Исторический<span>Томск</span>
-		</Link>
-		<button
-			className="hamburger"
-			onClick={() => {
-				setIsNavExpanded(!isNavExpanded)
-			}}
-		>
-			{isNavExpanded ? <FontAwesomeIcon icon="fa-solid fa-bars" />
-				: <FontAwesomeIcon icon='fas fa-close' />}
-		</button>
-		<div
-			className={
-				isNavExpanded ? "navigation-menu expanded" : "navigation-menu"
-			}
-		>
-			<ul className='navbar-nav'>
-				<li className='nav-item'>
-					<Link to={`/`} className='nav-link'>
-						Главная
-					</Link>
-				</li>
-				<li className='nav-item'>
-					<Link to={`/AllHistPlacesPage`} className='nav-link'>
-						Исторические места
-					</Link>
-				</li>
-				<li className='nav-item'>
-					<Link to={`/about`} className='nav-link'>
-						О нас
-					</Link>
-				</li>
-			</ul>
-		</div>
-	</nav>)
+	return (
+		<nav className='navigation'>
+			<Link to={`/`} className='navbar-site-brand'>
+				Исторический<span>Томск</span>
+			</Link>
+			<button
+				className='hamburger'
+				onClick={() => {
+					setIsNavExpanded(!isNavExpanded)
+				}}
+			>
+				{!isNavExpanded ? (
+					<FontAwesomeIcon icon='fa-solid fa-bars' />
+				) : (
+					<FontAwesomeIcon icon='fa-solid fa-xmark' />
+				)}
+			</button>
+			<div
+				className={
+					isNavExpanded ? 'navigation-menu expanded' : 'navigation-menu'
+				}
+			>
+				<ul className='navbar-nav'>
+					<li className='nav-item'>
+						<Link to={`/`} className='nav-link'>
+							Главная
+						</Link>
+					</li>
+					<li className='nav-item'>
+						<Link to={`/AllHistPlacesPage`} className='nav-link'>
+							Исторические места
+						</Link>
+					</li>
+					<li className='nav-item'>
+						<Link to={`/about`} className='nav-link'>
+							О нас
+						</Link>
+					</li>
+				</ul>
+			</div>
+		</nav>
+	)
 }
